d074ad1340472c70684fc13c25e5f77a3ee1ccdb
Oleh Kuper <ok1804@ukr.net>
2025-07-19 19:48:21 +0300

    create singin test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,28 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('should signin with valid credentials', () => {
+    cy.login('tomsmith', 'SuperSecretPassword!');
+    cy.location('pathname').should('eq', '/secure');
+  });
 
+  it('should validate username', () => {
+    cy.login('tom', 'SuperSecretPassword!');
+    cy.get('#flash').contains('Your username is invalid!');
+  });
+
+  it('should validate user password', () => {
+    cy.login('tomsmith', 'SuperSecretPassword');
+    cy.get('#flash').contains('Your password is invalid!');
+  });
+
+  it('should logout from the app', () => {
+    cy.login('tomsmith', 'SuperSecretPassword!');
+    cy.location('pathname').should('eq', '/secure');
+    cy.contains('Logout').click();
+    cy.location('pathname').should('eq', '/login');
   });
 });
